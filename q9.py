bae7013a65c04c38671b1ba317ed7063881f45a8
Erysilva98 <erimilsonsilva98@gmail.com>
2022-04-15 11:25:11 -0300

    Git Lista 1

--- q9.py
+++
@@ -1,8 +0,0 @@
-# 9 - Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50. 
-
-i = 1
-
-for i in range(50):
-    num = i
-    if num % 2 != 0:
-        print('Número Impar Gerado: ',num)
